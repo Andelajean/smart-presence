5e067d761718bc1f0f28d2bd2f7942ef5f6cb13d
Andelajean <ajeangael@gmail.com>
2024-08-14 18:20:46 +0200

    mise a jour de quelques parties

--- app/src/main/java/com/example/gpresence/MainActivity.kt
+++ app/src/main/java/com/example/gpresence/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.gpresence
 
-//import AdminActivity
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
@@ -32,11 +35,8 @@ class MainActivity : AppCompatActivity() {
                     loadFragment(RequestsFragment())
                     true
                 }
-               /* R.id.nav_admin -> {
-
-                }*/
                 R.id.nav_admin -> {
-                   looadmain()
+                    loadAdmin()
                     true
                 }
                 else -> false
@@ -54,8 +54,11 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_container, fragment)
             .commit()
     }
-   private fun looadmain(){
+
+    private fun loadAdmin() {
         val intent = Intent(this, AdminActivity::class.java)
         startActivity(intent)
     }
+
+
 }
