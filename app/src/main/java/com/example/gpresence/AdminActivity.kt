f4fb92c6414f1123684b14e8b33ef44a97778d00
Andelajean <ajeangael@gmail.com>
2024-08-20 11:28:24 +0200

    mise de la partie administrateur

--- app/src/main/java/com/example/gpresence/AdminActivity.kt
+++ app/src/main/java/com/example/gpresence/AdminActivity.kt
@@ -1,64 +1,55 @@
 package com.example.gpresence
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class AdminActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var fragmentContainer: FrameLayout
-    private lateinit var imageButton: ImageButton
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_admin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        drawerLayout = findViewById(R.id.drawer_layout)
-        fragmentContainer = findViewById(R.id.fragment_container)
-        imageButton = findViewById(R.id.imageButton)
-        val navigationView: NavigationView = findViewById(R.id.navigation_view)
-
-        imageButton.setOnClickListener {
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START)
-            } else {
-                drawerLayout.openDrawer(GravityCompat.START)
-            }
-        }
-
-        navigationView.setNavigationItemSelectedListener { item ->
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_role -> replaceFragment(CodeFragment())
-                R.id.nav_etats_globaux -> replaceFragment(EtatsGlobaux())
-                R.id.nav_compte -> replaceFragment(CompteFragment())
-                R.id.nav_requete -> replaceFragment(VoirRequete())
-                R.id.nav_equipement->replaceFragment(Equipement())
+                R.id.nav_tableau -> {
+                    loadFragment(StatFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+                R.id.nav_equipement -> {
+                    loadFragment(Equipement())
+                    true
+                }
+                R.id.nav_code -> {
+                    loadFragment(CompteFragment())
+                    true
+                }
+                R.id.nav_voir_requete->{
+                    loadFragment(VoirRequete())
+                    true
+                }
+                else -> false
             }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            true
+        }
+
+        // Load the default fragment
+        if (savedInstanceState == null) {
+            loadFragment(StatFragment())
         }
     }
-
-    private fun replaceFragment(fragment: Fragment) {
-        fragmentContainer.visibility = View.VISIBLE
+    private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
+            .replace(R.id.fragment_containe, fragment)
             .commit()
     }
 }
--- app/src/main/java/com/example/gpresence/MainActivity.kt
+++ app/src/main/java/com/example/gpresence/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,7 +14,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
